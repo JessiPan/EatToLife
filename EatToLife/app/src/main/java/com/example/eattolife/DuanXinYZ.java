9002b9b16442804c953df241d6b7ed8ae0eb5c82
JessiePan <1779011055@qq.com>
2024-03-17 13:33:20 +0800

    海鑫楼

---
+++ EatToLife/app/src/main/java/com/example/eattolife/DuanXinYZ.java
@@ -0,0 +1,14 @@
+package com.example.eattolife;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DuanXinYZ extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_duan_xin_yz);
+    }
+}
---
+++ EatToLife/app/src/main/java/com/example/eattolife/YinShiTJ.java
@@ -0,0 +1,14 @@
+package com.example.eattolife;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class YinShiTJ extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_yin_shi_tj);
+    }
+}
