b904473df7969c1d9387147717371fc595a09d49
JessiePan <1779011055@qq.com>
2024-03-27 12:07:19 +0800

    增加back按钮

---
+++ EatToLife/app/src/main/java/com/example/eattolife/FontIconView.java
@@ -0,0 +1,41 @@
+package com.example.eattolife;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.graphics.Typeface;
+import android.util.AttributeSet;
+import android.widget.TextView;
+
+/**
+ * 创建： PengJunShan on 2019-02-26  14:52 描述：
+ */
+@SuppressLint("AppCompatCustomView")
+public class FontIconView extends TextView {
+
+    public FontIconView(Context context) {
+        super(context);
+        init(context);
+    }
+
+    public FontIconView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init(context);
+    }
+
+    public FontIconView(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        init(context);
+    }
+
+    /**
+     * 初始化
+     * @param context
+     */
+    private void init(Context context) {
+        //设置字体图标
+        Typeface font = Typeface.createFromAsset(context.getAssets(), "iconfont.ttf");
+        this.setTypeface(font);
+    }
+
+
+}
--- EatToLife/app/src/main/java/com/example/eattolife/YinShiTJ.java
+++ EatToLife/app/src/main/java/com/example/eattolife/YinShiTJ.java
@@ -2,7 +2,12 @@ package com.example.eattolife;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class YinShiTJ extends AppCompatActivity {
 
@@ -10,5 +15,21 @@ public class YinShiTJ extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_yin_shi_tj);
+
+        //获得图标
+        TextView back1 = findViewById(R.id.back);
+        Typeface font = Typeface.createFromAsset(getAssets(),"back.ttf");
+        back1.setTypeface(font);
+
+        //跳转按钮
+        Button back2 = findViewById(R.id.back);
+        back2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setClass(YinShiTJ.this, Wo.class);
+                startActivity(intent);
+            }
+        });
     }
 }
